2597516bb696e460aa88ffd81571dfc82e17b3b0
Heimy Dias <heimysantana@hotmail.com>
2024-02-15 21:38:36 -0300

    jpa repository, dependency injection, database seeding

---
+++ src/main/java/com/heimy/course/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.heimy.course.repositories;
+
+import com.heimy.course.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
