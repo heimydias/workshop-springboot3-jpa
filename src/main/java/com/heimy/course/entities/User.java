b826278232ec7088c477f5850fc80226cc5b735b
Heimy Dias <heimysantana@hotmail.com>
2024-02-15 21:12:21 -0300

    h2 database, test profile, jpa

--- src/main/java/com/heimy/course/entities/User.java
+++ src/main/java/com/heimy/course/entities/User.java
@@ -1,11 +1,20 @@
 package com.heimy.course.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
 import java.io.Serializable;
 
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue
     private Long id;
     private String name;
     private String email;
