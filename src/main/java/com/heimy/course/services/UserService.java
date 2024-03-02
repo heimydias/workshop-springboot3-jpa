19c8492cbe773efb426a5e9c87f92cf861780c3c
Heimy Dias <heimysantana@hotmail.com>
2024-03-01 21:01:17 -0300

    exception handling delete - delete

--- src/main/java/com/heimy/course/services/UserService.java
+++ src/main/java/com/heimy/course/services/UserService.java
@@ -2,8 +2,11 @@ package com.heimy.course.services;
 
 import com.heimy.course.entities.User;
 import com.heimy.course.repositories.UserRepository;
+import com.heimy.course.services.exceptions.DatabaseException;
 import com.heimy.course.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -29,7 +32,13 @@ public class UserService {
     }
 
     public void delete(Long id){
-        userRepository.deleteById(id);
+        findById(id);
+        try {
+            userRepository.deleteById(id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DatabaseException(e.getMessage());
+        }
+
     }
 
     public User update(Long id, User obj) {
---
+++ src/main/java/com/heimy/course/services/exceptions/DatabaseException.java
@@ -0,0 +1,15 @@
+package com.heimy.course.services.exceptions;
+
+import java.io.Serial;
+
+public class DatabaseException extends RuntimeException {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    public DatabaseException(String msg) {
+        super(msg);
+    }
+
+
+}
